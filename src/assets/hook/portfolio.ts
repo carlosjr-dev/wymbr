ef0e5be43eb1f7b0435e670fc8448d9c4156876c
junior <wolfnygard@gmail.com>
2023-10-30 10:38:57 -0300

    carrossel

--- src/assets/hook/portfolio.ts
+++ src/assets/hook/portfolio.ts
@@ -1,3 +1,4 @@
+export { portfolio };
 interface propsPort {
   id: number;
   url: string;
@@ -10,28 +11,28 @@ const portfolio: propsPort[] = [
   {
     id: 1,
     title: "nome1",
-    url: "../../../../imgPort/img1.jpg",
+    url: "https://images.unsplash.com/photo-1695970921584-bb052e24916f?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D",
     icon1: "UI",
     icon2: "Web",
   },
   {
     id: 2,
     title: "nome2",
-    url: "../../../../imgPort/img1.jpg",
+    url: "https://images.unsplash.com/photo-1690284331959-c3b9430e7620?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D",
     icon1: "UI",
     icon2: "Web",
   },
   {
     id: 3,
     title: "nome3",
-    url: "../../../../imgPort/img1.jpg",
+    url: "https://images.unsplash.com/photo-1654751647618-fdaa41d2739a?auto=format&fit=crop&q=80&w=1977&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     icon1: "UI",
     icon2: "Web",
   },
   {
     id: 4,
     title: "nome4",
-    url: "../../../../imgPort/img1.jpg",
+    url: "https://plus.unsplash.com/premium_photo-1695416270424-ce9299edd102?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D",
     icon1: "UI",
     icon2: "Web",
   },
@@ -39,16 +40,36 @@ const portfolio: propsPort[] = [
   {
     id: 5,
     title: "nome5",
-    url: "../../../../imgPort/img1.jpg",
+    url: "https://images.unsplash.com/photo-1691036365017-76f8114f50c4?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D",
     icon1: "UI",
     icon2: "Web",
   },
   {
     id: 6,
     title: "nome6",
-    url: "../../../../imgPort/img1.jpg",
+    url: "https://plus.unsplash.com/premium_photo-1674204005101-a1cd570f42b3?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D",
+    icon1: "UI",
+    icon2: "Web",
+  },
+  {
+    id: 7,
+    title: "nome6",
+    url: "https://plus.unsplash.com/premium_photo-1674204005101-a1cd570f42b3?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D",
+    icon1: "UI",
+    icon2: "Web",
+  },
+  {
+    id: 8,
+    title: "nome6",
+    url: "https://plus.unsplash.com/premium_photo-1674204005101-a1cd570f42b3?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D",
+    icon1: "UI",
+    icon2: "Web",
+  },
+  {
+    id: 9,
+    title: "nome6",
+    url: "https://plus.unsplash.com/premium_photo-1674204005101-a1cd570f42b3?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D",
     icon1: "UI",
     icon2: "Web",
   },
 ];
-export default portfolio;
--- src/assets/pages/portfolio/Portfolio.tsx
+++ src/assets/pages/portfolio/Portfolio.tsx
@@ -1,12 +1,77 @@
+import { MouseEvent, useRef } from "react";
+import { portfolio } from "../../hook/portfolio";
+
 import styles from "./Portfolio.module.css";
 
 function Portfolio() {
+  const carrossel = useRef<HTMLDivElement>(null);
+
+  const handdleAdd = (e: MouseEvent) => {
+    carrossel.current.scrolLeft += 10;
+  };
   return (
     <>
       <section className={styles.portfolioContent}>
-        <div className={styles.portfolioNav}>
-          <h1></h1>
+        <div className={styles.portfolioNav} ref={carrossel}>
+          {portfolio.map((item, index) => (
+            <div
+              style={{
+                backgroundImage: `url(${item.url})`,
+                backgroundSize: `cover`,
+                borderRadius: `10px`,
+                marginLeft: `15px`,
+                width: `273px`,
+                height: `340px`,
+                display: `flex`,
+                alignItems: `flex-end`,
+                gap: `8px`,
+                padding: `10px 10px`,
+                flex: `none`,
+              }}
+            >
+              <div className={styles.textAndIcon}>
+                <p
+                  key={index}
+                  style={{
+                    fontSize: `1.2rem`,
+                    color: `#fff`,
+                    margin: `10px 16px`,
+                    textShadow: ` 1px 1px #00000058`,
+                  }}
+                >
+                  {item.title}
+                </p>
+                <div className={styles.test}>
+                  <p
+                    style={{
+                      textShadow: ` 1px 1px #00000058`,
+                      color: `#fff`,
+                      marginLeft: `16px`,
+                      border: `1px solid #ffffff61`,
+                      borderRadius: `100px`,
+                      padding: `4px 20px `,
+                    }}
+                  >
+                    {item.icon1}
+                  </p>
+                  <p
+                    style={{
+                      textShadow: ` 1px 1px #00000058`,
+                      color: `#fff`,
+                      marginLeft: `16px`,
+                      border: `1px solid #ffffff61`,
+                      borderRadius: `100px`,
+                      padding: `4px 20px `,
+                    }}
+                  >
+                    {item.icon2}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
+        <button onClick={handdleAdd}>Testar</button>
       </section>
     </>
   );
